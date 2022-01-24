074cf9d8229da20ae6a185b1d812f8e7d970b85b
Artur <platar@lavait.com>
2022-01-24 13:26:44 +0100

    Added H2

---
+++ src/main/java/info/lavait/restapi/RestapiApplication.java
@@ -0,0 +1,13 @@
+package info.lavait.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+}
